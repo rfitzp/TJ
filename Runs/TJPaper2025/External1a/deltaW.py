016af4edcfdaacaa56b93f88b981a7d736edb04c
Richard Fitzpatrick <rfitzp1632@gmail.com>
2025-04-05 19:25:53 -0500

    Update deltaW.py

--- Runs/TJPaper2025/External1a/deltaW.py
+++ Runs/TJPaper2025/External1a/deltaW.py
@@ -56,12 +56,12 @@ b0 = []
 for p in pc:
     b0.append(0.08*p)
 
-pmin = 0.
+pmin = -1.e-4
 pmax = 1.5e-2
 ymax = 1.e-6
     
 fig = plt.figure (figsize = (8.0, 6.0))
-fig.canvas.manager.set_window_title (r'Circular: q_0=1.5: p_sig=1.315, epsilon_a=0.2')
+fig.canvas.manager.set_window_title (r'Circular: q_0=1.5: p_sig=1.31, epsilon_a=0.2')
 plt.rc ('xtick', labelsize = 15) 
 plt.rc ('ytick', labelsize = 15) 
 
@@ -70,18 +70,18 @@ plt.subplot (1, 1, 1)
 plt.xlim (pmin, pmax)
 #plt.ylim (-10., 20000.)
 
-plt.plot (b0, Wp,  color = 'blue',  linewidth = 1,  linestyle = 'dotted', marker = 's', fillstyle = 'none', markersize = 5, label = '$\delta W_p$ (STRIDE)')
-plt.plot (b0, Wp1, color = 'blue',  linewidth = 1,  linestyle = 'dotted', marker = 'x', fillstyle = 'none', markersize = 5, label = '$\delta W_p$ (TJ)')
-plt.plot (b0, Wv,  color = 'red',   linewidth = 1,  linestyle = 'dotted', marker = 's', fillstyle = 'none', markersize = 5, label = '$\delta W_v$ (STRIDE)')
-plt.plot (b0, Wv1, color = 'red',   linewidth = 1,  linestyle = 'dotted', marker = 'x', fillstyle = 'none', markersize = 5, label = '$\delta W_v$ (TJ)')
-plt.plot (b0, Wt,  color = 'black', linewidth = 1,  linestyle = 'dotted', marker = 's', fillstyle = 'none', markersize = 5, label = '$\delta W_t$ (STRIDE)')
-plt.plot (b0, Wt1, color = 'black', linewidth = 1,  linestyle = 'dotted', marker = 'x', fillstyle = 'none', markersize = 5, label = '$\delta W_t$ (TJ)')
-plt.axhline (0.,            color = 'black', linewidth = 1.5, linestyle = 'dotted')
+plt.plot (b0, Wp,  color = 'blue',  linewidth = 1,   linestyle = 'dotted', marker = 's', fillstyle = 'none', markersize = 5, label = '$\delta W_p$ (STRIDE)')
+plt.plot (b0, Wp1, color = 'blue',  linewidth = 1,   linestyle = 'dotted', marker = 'x', fillstyle = 'none', markersize = 5, label = '$\delta W_p$ (TJ)')
+plt.plot (b0, Wv,  color = 'red',   linewidth = 1,   linestyle = 'dotted', marker = 's', fillstyle = 'none', markersize = 5, label = '$\delta W_v$ (STRIDE)')
+plt.plot (b0, Wv1, color = 'red',   linewidth = 1,   linestyle = 'dotted', marker = 'x', fillstyle = 'none', markersize = 5, label = '$\delta W_v$ (TJ)')
+plt.plot (b0, Wt,  color = 'black', linewidth = 1,   linestyle = 'dotted', marker = 's', fillstyle = 'none', markersize = 5, label = '$\delta W_t$ (STRIDE)')
+plt.plot (b0, Wt1, color = 'black', linewidth = 1,   linestyle = 'dotted', marker = 'x', fillstyle = 'none', markersize = 5, label = '$\delta W_t$ (TJ)')
+plt.axhline (0.,   color = 'black', linewidth = 1.5, linestyle = 'dotted')
 
-plt.xlabel (r'$\beta_0$',    fontsize = "15")
+plt.xlabel (r'$\beta_0$', fontsize = "15")
 plt.legend (fontsize = "12")
 
 plt.tight_layout ()
 
-#plt.show ()    
-plt.savefig("deltaW.pdf")
+plt.show ()    
+#plt.savefig("deltaW.pdf")
